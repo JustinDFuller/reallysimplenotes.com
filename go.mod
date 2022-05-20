19143d9860d5efc833ed8ddeef73f2ba32020d42
Justin Fuller <justindanielfuller@gmail.com>
2022-05-20 09:00:05 -0400

    App engine/Go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/justindfuller/really-simple-notes
+
+go 1.15
+
+require (
+	github.com/cespare/reflex v0.3.1 // indirect
+	golang.org/dl v0.0.0-20220510203206-88ea6714b1d9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+}
