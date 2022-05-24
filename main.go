5d83ede3b868e8df69e622b513f5f09de5e1583f
Justin Fuller <justindanielfuller@gmail.com>
2022-05-24 09:15:50 -0400

    meta tags

--- main.go
+++ main.go
@@ -6,11 +6,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.URL.Path, "/public") {
+			path := fmt.Sprintf(".%s", r.URL.Path)
+			log.Printf("Serving static file: %s", path)
+			http.ServeFile(w, r, path)
+			return
+		}
+
 		var t = template.Must(template.New("main.template.html").ParseFiles("main.template.html"))
 
 		var css []byte
